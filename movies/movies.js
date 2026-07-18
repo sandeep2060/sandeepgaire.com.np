d62c98a452e499ec7250ef4ec6ab1b01c28218ca
sandeep2060 <sandeep2060@users.noreply.github.com>
2026-07-18 22:05:55 +0000

    deploy: e2e35db8f9f757c4a3cba5127242c6754ce3ab2b

--- movies/movies.js
+++ movies/movies.js
@@ -1,17 +1,17 @@
 const moviesData = [
   {
     id: 1,
-    title: "Dune",
-    year: 2021,
-    genre: "Sci-Fi",
-    rating: 8.0,
-    poster: "https://picsum.photos/seed/dune/300/450",
+    title: "Pritam and Pedro",
+    year: 2026,
+    genre: "cybercrime comedy-thriller and buddy-cop drama",
+    rating: IMDb 7.8/10 || TMDB 8.6/10,
+    poster: "https://img1.hotstarext.com/image/upload/f_auto/sources/r1/cms/prod/380/1782998630380-h",
     backdrop: "https://picsum.photos/seed/dune-back/1280/720",
     description: "A noble family becomes embroiled in a war for control over the galaxy's most valuable asset.",
     duration: "2h 35m",
     director: "Denis Villeneuve",
     cast: ["Timothée Chalamet", "Rebecca Ferguson", "Oscar Isaac"],
-    trailerUrl: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+    trailerUrl: "https://mega.nz/file/m7AXWBgI#i8qSF3q9r0xgTiibF1bqp2q5i4BPzpQ77P34UZU_CDk",
     categories: ["trending", "popular", "topRated", "recommended"],
     tags: ["sci-fi", "adventure"]
   },
