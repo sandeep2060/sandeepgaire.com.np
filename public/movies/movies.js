1a5a68427325547d153eb126bb7e1f9c34f2b10a
sandeep2060 <103778388+sandeep2060@users.noreply.github.com>
2026-07-18 10:41:40 +0545

    Create movies.js

---
+++ public/movies/movies.js
@@ -0,0 +1,1 @@
+const movies=[{title:"Pritam",poster:"https://via.placeholder.com/220x320?text=Pritam",video:"https://example.com/video.mp4"},{title:"Pedro",poster:"https://via.placeholder.com/220x320?text=Pedro",video:"https://example.com/video2.mp4"}];
